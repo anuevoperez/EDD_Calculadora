37c61bd04f138592f22c5da0d2eb76b26775bfa9
Carlos Fuentes <c.fvazquez@hotmail.com>
2021-02-23 19:04:10 +0100

    Primer metodo implementado

--- src/Actividad_2/Resta.java
+++ src/Actividad_2/Resta.java
@@ -1,5 +1,21 @@
 package Actividad_2;
 
 public class Resta {
-
+	
+	/** 
+	 * <b>Método 1:</b>  Resta de dos números reales, tendrá 2 parámetros de entrada y uno de salida, que será la solución.
+	 *  
+	 * @param num1 
+	 * 			Primer número a restar que introduce el usuario (double)
+	 * @param num2 
+	 * 			Segundo número a restar que introduce el usuario (double)
+	 *
+	 * @return <b>rest1</b> Devuelve la resta de num1 y num2
+	 * 		
+	 */
+	
+	public double restar1 (double num1, double num2) {
+		return num1 - num2;
+	}
+	
 }
