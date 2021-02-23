0ecaf1485675be2715af460d80ce431c6948a4f4
Carlos Fuentes <c.fvazquez@hotmail.com>
2021-02-23 19:08:51 +0100

    Segundo metodo implementado

--- src/Actividad_2/Resta.java
+++ src/Actividad_2/Resta.java
@@ -18,4 +18,23 @@ public class Resta {
 		return num1 - num2;
 	}
 	
+	/** 
+	 * <b>Método 2:</b>  Resta de dos números enteros, tendrá 2 parámetros de entrada y uno de salida que será la solución.
+	 *  
+	 * @param num1 
+	 * 			Primer número a restar que introduce el usuario (int)
+	 * @param num2 
+	 * 			Segundo número a restar que introduce el usuario (int)
+	 *
+	 * @return res2 
+	 * 			Devuelve la resta de num1 y num2 (int)
+	 * 
+	 */
+	
+	public int restar2 (int num1, int num2) {
+		return num1 - num2;
+	}
+	
+	
+	
 }
