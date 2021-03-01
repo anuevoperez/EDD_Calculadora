19416a24688d38901d0c0217608db2b0a86d8d9a
raullopeslopez <raullopeslopez@gmail.com>
2021-03-01 23:21:33 +0100

    Documentación de testSuma

--- test/TestSuma.java
+++ test/TestSuma.java
@@ -3,7 +3,23 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import Actividad_2.Suma;
 
+/**
+ * Esta batería de pruebas unitarias comprueba que cada uno de los métodos de la clase Suma 
+ * arrojan el resultado esperado. Para ello, se ha comprobado con diferentes parámetros de 
+ * entrada.
+ * 
+ * Cabe tener en cuenta como instrucciones genéricas que cada vez que se realiza la prueba 
+ * unitaria de un método, se instancia un objeto diferente para obtener resultado más próximos
+ * a la realidad
+ *  
+ * @author: <b>Carlos Fuentes Vázquez</b>
+ * @version: 1.0
+ * 
+ */
+
 class TestSuma {
+
+	// Prueba unitaria método 1 de la clase División @see <b>Suma</b>
 	
 	@Test
 	public void testSumar1() {
@@ -19,20 +35,36 @@ class TestSuma {
 		assertEquals(s.sumar1(-1.25, -3), -4.25);
 		assertEquals(s.sumar1(-1.96, -4.61), -6.57);
 	}
+
+	// Prueba unitaria método 1 de la clase División @see <b>Suma</b>
 	
 	@Test
 	public void testSumar2 () {
 		Suma s = new Suma ();
+
+		/**
+		 * Con assertEquals esperamos en el resultado coincida con el valor esperado
+		 * Probamos con números positivos, negativos, y con decimales
+		 * 
+		 */
 		
 		assertEquals(s.sumar2(2, 5), 7);
 		assertEquals(s.sumar2(-4, 5), 1);
 		assertEquals(s.sumar2(-6, 4), -2);
 		assertEquals(s.sumar2(-7, -9), -16);
 	}
+
+	// Prueba unitaria método 1 de la clase División @see <b>Suma</b>
 	
 	@Test
 	public void testSumar3 () {
 		Suma s = new Suma ();
+
+		/**
+		 * Con assertEquals esperamos en el resultado coincida con el valor esperado
+		 * Probamos con números positivos, negativos, y con decimales
+		 * 
+		 */
 		
 		assertEquals(s.sumar3(2, 5, 9), 16);
 		assertEquals(s.sumar3(-2, 5, 9), 12);
@@ -49,10 +81,18 @@ class TestSuma {
 		assertEquals(s.sumar3(2, 0, 9), 11);
 		assertEquals(s.sumar3(2, 5, 0), 7);
 	}
+
+	// Prueba unitaria método 1 de la clase División @see <b>Suma</b>
 	
 	@Test
 	public void testSumar4 () {
 		Suma s = new Suma ();
+
+		/**
+		 * Con assertEquals esperamos en el resultado coincida con el valor esperado
+		 * Probamos con números positivos, negativos, y con decimales
+		 * 
+		 */
 		
 		assertEquals(s.sumar4(0), 0);
 		assertEquals(s.sumar4(3), 3);
